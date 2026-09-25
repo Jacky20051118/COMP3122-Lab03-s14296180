efd2fb1f1149498006a5386b86b2b404ebe12c91
Jacky20051118 <kongchingkit@gmail.com>
2026-09-25 18:01:28 +0800

    Shape.java

---
+++ Shape.java
@@ -0,0 +1,19 @@
+public class Shape {
+    private double x, y;
+
+    public Shape(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() { return x; }
+    public double getY() { return y; }
+
+    public double perimeter() {
+        return 0;
+    }
+
+    public double area() {
+        return 0;
+    }
+}
